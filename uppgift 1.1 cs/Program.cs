222316a128164a887ea54c300707a68ce7ecc361
Pingvu <nhlpool27@gmail.com>
2023-01-18 10:09:51 +0100

    Add project files.

---
+++ uppgift 1.1 cs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace uppgift2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.WriteLine("\n your computer has terminated with a critical error" + "\n\n error code c2531b");
+        }
+    }
+}
